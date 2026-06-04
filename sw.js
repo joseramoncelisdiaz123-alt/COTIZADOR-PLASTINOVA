dce2c44a6640da7357f5501be623ed0fc00b3aef
joseramoncelisdiaz123-alt <joseramoncelisdiaz123@gmail.com>
2026-06-03 20:20:43 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,27 @@
-// Este archivo es OBLIGATORIO para que Android deje instalar la App
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'plastinova-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.jpeg'
+];
+
+// Instalar el Service Worker y guardar en caché los archivos base
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptar las peticiones para que funcione sin conexión
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
